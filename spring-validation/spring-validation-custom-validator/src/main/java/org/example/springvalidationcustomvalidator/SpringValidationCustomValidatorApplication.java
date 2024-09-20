e97235c03dcfafe3e3585b432faf075089452f34
wjh <wjh@travelsky.com.cn>
2024-09-20 10:24:58 +0800

    初始化custom-validator

---
+++ spring-validation/spring-validation-custom-validator/src/main/java/org/example/springvalidationcustomvalidator/SpringValidationCustomValidatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.springvalidationcustomvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationCustomValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringValidationCustomValidatorApplication.class, args);
+	}
+
+}
---
+++ spring-validation/spring-validation-custom-validator/src/test/java/org/example/springvalidationcustomvalidator/SpringValidationCustomValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springvalidationcustomvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationCustomValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
